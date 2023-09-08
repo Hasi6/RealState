21ce3b6d3d20dd17be4f8ea15fa6fee30eb3e7d7
Hasi6 <hasitha.chandula@gmail.com>
2023-09-08 23:24:08 +0530

    [add]: setup jest

---
+++ frontend/src/app/dashboard/index.test.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import { render, screen } from '@testing-library/react';
+import Button from './page';
+
+test('renders a button with the correct text', () => {
+  render(<Button />);
+  const buttonElement = screen.getByText('Dashboard');
+  expect(buttonElement).toBeDefined();
+});
